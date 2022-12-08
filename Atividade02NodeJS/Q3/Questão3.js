f9f96f7c2501beaba0e7d26f436fa38972395687
Gabriel Morandi de Mello <61758357+gabrielmorandi@users.noreply.github.com>
2022-12-08 06:13:39 -0300

    add Q3.js da Atividade 2

--- Atividade02NodeJS/Q3/Questão3.js
+++ Atividade02NodeJS/Q3/Questão3.js
@@ -1,4 +1,56 @@
-
 import fs from 'fs'
 
+const dir = 'Diretório Exemplo' // esse é o caminho RELATIVO(ou seja, dentro do repositório que esse arquivo está inserido) do repositório que você quer saber oque tem dentro
+const saida = `Oque tem dentro do ${dir}: \n--------------------------------------\n> ${dir}`
+var b = ''
 
+function verificaSubDir(filename ,dirname) {
+    fs.stat(`./${dir}/${filename}/${dirname}`, (err, stats) => {
+        if (err) {
+            console.log(err)
+        } else {
+            if (stats.isFile() === true ) {
+                // nada
+            } else {
+                var a = filename
+                if (a != b) {
+                    b = a
+                    console.log('    > /' + filename)   
+                }
+                console.log('        >/' + dirname)
+            }
+        }
+    })
+}
+
+function verificaDir(filename) {
+    fs.stat(`./${dir}/${filename}`, (err, stats) => {
+        if (err) {
+            console.log(err)
+        } else {
+            if (stats.isFile() === true) {
+                // nada
+            } else {
+                fs.promises.readdir(process.cwd() + `//${dir}/${filename}`)
+                .then(dirnames => {
+                    for (let dirname of dirnames) {
+                        verificaSubDir(filename, dirname)
+                    }
+                })
+            }
+        }
+    })
+}
+
+function diretorio() {
+    fs.promises.readdir(process.cwd() + `//${dir}`)
+    .then(filenames => {
+        console.log(saida)
+        for (let filename of filenames) {
+            verificaDir(filename)
+        }
+    }) 
+    
+}
+
+diretorio()
