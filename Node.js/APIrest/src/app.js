153ed19c219e8687a8ada40e3259b1b5eaf9ffb7
Gustavo Machado <contact.gustavomachado@gmail.com>
2023-01-18 10:34:00 -0300

    Aula18-01-23 :)

--- Node.js/APIrest/src/app.js
+++ Node.js/APIrest/src/app.js
@@ -2,15 +2,13 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.json())
+
 const livros = [
 
-    {
-
-    id: 1, "titulo": "A volta dos que não foram",
-    id: 2, "titulo": "Pequeno Príncipe",
-    id: 3, "titulo": "Como perder uma copa" 
-
-    }
+    {id: 1, "titulo": "A volta dos que não foram"},
+    {id: 2, "titulo": "Pequeno Príncipe"},
+    { id: 3, "titulo": "Como perder uma copa"}
 
 ]
 
@@ -20,10 +18,63 @@ app.get('/',(req,res) => {
 
 })
 
+
+//GET NO OBJETO
 app.get('/Livros',(req,res) => {
 
     res.status(200).json(livros)
 
 })
 
+//GET EM UM INDEX
+app.get('/Livros/:id',(req,res) => {
+
+    let index = buscaLivro(req.params.id)
+    res.json(livros[index])
+
+})
+
+//ENVIAR NOVO ARQUIVO JSON
+app.post('/Livros',(req,res) => {
+
+    livros.push(req.body)
+    res.status(201).send('O livro foi cadastrado com sucesso!')
+
+})
+
+//ALTERAR TITULO COM BASE NO INDEX
+app.put('/Livros/:id',(req,res) => {
+
+    let index = buscaLivro(req.params.id)
+    
+    livros[index].titulo = req.body.titulo
+
+    res.json(livros)
+
+})
+
+function buscaLivro(id){
+    return livros.findIndex(livro => livro.id == id)
+}
+
+//DELETAR ARQUIVO JSON
+app.delete('/Livros/:id',(req,res) => {
+
+    let {id} = req.params ///PEGANDO ID DA REQUISICAO
+    let index = buscaLivro(id) //VERIFICANDO E VINCULANDO ID A VARIAVEL
+    
+    livros.splice(index,1)
+
+    res.status(201).send(`O livro ${livros[index].titulo} foi deletado com sucesso!`)
+
+})
+
 export default app
+
+//CRUD
+//CREATE
+//READ
+//UPDATE
+//DELETE
+
+// MONGO DB ATLAS
